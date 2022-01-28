b581b8f8c2a8d02a208ee396102c01ce2c300191
Gabriel Leitão <xgabriel.rf@gmail.com>
2022-01-28 15:43:10 -0300

    starting jenkins lab

---
+++ jenkins-lab/provision.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+echo "Installing Jenkins and dependencies..."
+sudo yum install git wget epel-release java-11-openjdk-devel -y
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+echo "Installing Docker and Docker-Compose..."
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo systemctl daemon-reload
+
+echo "Access to the Jenkins user..."
+sudo usermod -aG docker jenkins
+sudo chmod 666 /var/run/docker.sock
